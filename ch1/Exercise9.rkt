7521e0365b542d26e21ec0cdcc4546693ce31285
jesseck3013 <jesseck3013@outlook.com>
2025-05-30 10:35:59 +0800

    added ch1

---
+++ ch1/Exercise9.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise5) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+(define cat (bitmap "../resources/cat1.png"))
+
+;(define in -10)
+;(define in "hh")
+(define in cat)
+
+(if (image? in) (* (image-width in) (image-height in))
+    (if (string? in) (string-length in)
+        (if (number? in) 10 20)))
