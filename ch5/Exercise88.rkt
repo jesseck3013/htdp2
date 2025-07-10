2bd1d1dcd680ea54c8a64e2bb46daca0bf29bea4
jesseck3013 <jesseck3013@outlook.com>
2025-07-10 10:49:39 +0800

    added 88

---
+++ ch5/Exercise88.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname data_info) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; A VCat is a structure:
+; (make-VCat Number Number)
+; interpretation (make-VCat x h) describes a virtual cat
+; whose x-cordinate is x and its hapiness is h.
+(define-struct VCat [x happiness])
+
