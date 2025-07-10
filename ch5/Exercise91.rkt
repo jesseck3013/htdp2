0cb026b989d93f2c68da325f3139b092ccaa9ecc
jesseck3013 <jesseck3013@outlook.com>
2025-07-10 11:29:42 +0800

    added 91

---
+++ ch5/Exercise91.rkt
@@ -0,0 +1,51 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname data_info) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+(define cat1 (bitmap "../resources/cat1.png"))
+
+; A VCat is a structure:
+; (make-VCat Number Number)
+; interpretation (make-VCat x h d) describes a virtual cat
+; whose x-cordinate is x, hapiness is h, and direction is d.
+(define-struct VCat [x happiness direction])
+
+(define BACKGROUND (empty-scene (* (image-width cat1) 10)
+                                (* (image-height cat1) 1.5)))
+
+(define Y-AXIS (/ (image-height BACKGROUND) 2))
+(define BG-WIDTH 20)
+; VCat -> Image
+; render a VCat on an empty background image.
+(define (render vcat)
+  (beside
+   (rectangle BG-WIDTH (VCat-happiness vcat) "solid" "red")
+   (place-image cat1 (VCat-x vcat) Y-AXIS BACKGROUND)))
+
+; VCat -> VCat
+; VCat moves 3 pixels right, 
+(define (clock-tick-handler cat)
+  (make-VCat
+   (if (string=? (VCat-direction cat) "right")
+        (+ (VCat-x cat) 3)
+        (- (VCat-x cat) 3))
+  (cond
+    [(<= (- (VCat-happiness cat) 0.1) 0) 0]
+    [else (- (VCat-happiness cat) 0.1)])
+  (cond
+    [(>= (VCat-x cat) (image-width BACKGROUND)) "left"]
+    [(<= (VCat-x cat) 0) "right"]
+    [else (VCat-direction cat)])))
+
+(check-expect (clock-tick-handler (make-VCat 10 100 "right"))
+              (make-VCat 13 99.9 "right"))
+
+(define (should-stop vcat)
+  (= (VCat-happiness vcat) 0))
+
+(define (happy-cat vcat)
+  (big-bang vcat
+   [to-draw render]
+   [on-tick clock-tick-handler]
+   [stop-when should-stop]))
+
+(happy-cat (make-VCat 0 100 "right"))
