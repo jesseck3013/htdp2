daa875a77c3daa4a685deb2d0a7947da7a862522
jesseck3013 <jesseck3013@outlook.com>
2025-07-20 16:36:07 +0800

    added 113

---
+++ ch6/Exercise113.rkt
@@ -0,0 +1,37 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise97) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; A SIGS is one of: 
+; – (make-aim UFO Tank)
+; – (make-fired UFO Tank Missile)
+; interpretation represents the complete state of a 
+; space invader game
+
+(define (SIGS? input)
+  (or (aim? input)
+      (fired? input)))
+
+; A Coordinate is one of: 
+; – a NegativeNumber 
+; interpretation on the y axis, distance from top
+; – a PositiveNumber 
+; interpretation on the x axis, distance from left
+; – a Posn
+; interpretation an ordinary Cartesian point
+(define (Coordinate? input)
+  (cond 
+    [(and
+      (number? input)
+      (not (= input 0))) #true]
+    [(posn? input) #true]
+    [else #false]))
+
+; A VAnimal is either
+; – a VCat
+; – a VCham
+(define (VAnimal? input)
+  (or (VCat? input)
+      (VCham? input)))
+  
+
