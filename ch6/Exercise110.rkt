9606519953545e8f25ea8f660d8fb8f6d1ec91ab
jesseck3013 <jesseck3013@outlook.com>
2025-07-20 16:21:30 +0800

    added 110

---
+++ ch6/Exercise110.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise97) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; Any -> Number
+; computes the area of a disk with radius v, 
+; if v is a number
+(define (checked-area-of-disk v)
+  (cond
+    [(and (number? v) (> v 0)) (area-of-disk v)]
+    [else (error "area-of-disk: number expected")]))
