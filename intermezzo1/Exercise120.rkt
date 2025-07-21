470fd8c1bf20f4009b7e6e5c6f41d1c2d21aaa3b
jesseck3013 <jesseck3013@outlook.com>
2025-07-21 11:44:59 +0800

    added 120

---
+++ intermezzo1/Exercise120.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise97) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; 1. (x)
+; (variable) is illegal
+
+; 2. (+ 1 (not x))
+; (primitive value (primitive variable))
+; (primitive expr (primitive expr))
+; (primitive expr expr)
+; expr
+
+; 3. (+ 1 2 3)
+; (primitive value value value)
+; (primitive expr expr expr)
+; expr
