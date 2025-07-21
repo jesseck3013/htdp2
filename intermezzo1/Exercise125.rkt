178063564a07961560484279e1a0641074f973f7
jesseck3013 <jesseck3013@outlook.com>
2025-07-21 12:19:58 +0800

    added 136

---
+++ intermezzo1/Exercise125.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise97) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+(define-struct oops [])
+; legal
+
+(define-struct child [parents dob date])
+; legal
+
+(define-struct (child person) [dob date])
+; illegal
+; vaild structure is (define-struct c [s-1 ... s-n])
+; where c is not in parentheses
