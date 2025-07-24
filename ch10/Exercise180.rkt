ba8f7ef0251d03c6c90285ecf60709894ab0b500
jesseck3013 <jesseck3013@outlook.com>
2025-07-24 22:36:17 +0800

    add 180

---
+++ ch10/Exercise180.rkt
@@ -0,0 +1,35 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise153) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+(define-struct editor [pre post])
+; An Editor is a structure:
+;   (make-editor Lo1S Lo1S) 
+; An Lo1S is one of: 
+; – '()
+; – (cons 1String Lo1S)
+
+(define HEIGHT 20) ; the height of the editor 
+(define WIDTH 200) ; its width 
+(define FONT-SIZE 16) ; the font size 
+(define FONT-COLOR "black") ; the font color 
+ 
+(define MT (empty-scene WIDTH HEIGHT))
+(define CURSOR (rectangle 1 HEIGHT "solid" "red"))
+
+(check-expect
+ (editor-text
+  (cons "p" (cons "o" (cons "s" (cons "t" '())))))
+ (text "post" FONT-SIZE FONT-COLOR))
+
+(define (my-implode s)
+   (cond
+     [(empty? s) ""]
+     [else (string-append
+            (first s)
+            (my-implode (rest s)))]))
+
+(define (editor-text s)
+  (text (my-implode s) FONT-SIZE FONT-COLOR))
+
+  
