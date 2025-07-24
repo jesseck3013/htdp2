d1a4370dd1b4060538cef09fe7a2c905c2407290
jesseck3013 <jesseck3013@outlook.com>
2025-07-24 21:44:00 +0800

    add 177

---
+++ ch10/10.4.rkt
@@ -0,0 +1,47 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise153) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+(define-struct editor [pre post])
+; An Editor is a structure:
+;   (make-editor Lo1S Lo1S) 
+; An Lo1S is one of: 
+; – '()
+; – (cons 1String Lo1S)
+
+(define good
+  (cons "g" (cons "o" (cons "o" (cons "d" '())))))
+(define all
+  (cons "a" (cons "l" (cons "l" '()))))
+(define lla
+  (cons "l" (cons "l" (cons "a" '()))))
+ 
+; data example 1: 
+(make-editor all good)
+ 
+; data example 2:
+(make-editor lla good)
+
+; Lo1s -> Lo1s 
+; produces a reverse version of the given list 
+
+(check-expect
+ (rev (cons "a" (cons "b" (cons "c" '()))))
+ (cons "c" (cons "b" (cons "a" '()))))
+
+(define (rev l)
+  (cond
+    [(empty? l) '()]
+    [else
+     (insert-inner (rev (rest l)) (first l))]))
+
+; Lo1s -> Lo1s
+(check-expect (insert-inner (cons "c" (cons "b" '())) "a")
+              (cons "c" (cons "b" (cons "a" '()))))
+(define (insert-inner l c)
+  (cond
+    [(empty? l) (cons c '())]
+    [else (cons (first l)
+                (insert-inner (rest l) c))]))
+
+  
---
+++ ch10/Exercise177.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise153) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+(define-struct editor [pre post])
+; An Editor is a structure:
+;   (make-editor Lo1S Lo1S) 
+; An Lo1S is one of: 
+; – '()
+; – (cons 1String Lo1S)
+  
+; String String -> Editor
+(check-expect (create-editor "hello" "world")
+              (make-editor (reverse (explode "hello"))
+                           (explode "world")))
+(define (create-editor left right)
+  (make-editor (reverse (explode left))
+               (explode right)))
