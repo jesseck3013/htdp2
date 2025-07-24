d3ba0c780252390352d57855a019c57520911127
jesseck3013 <jesseck3013@outlook.com>
2025-07-24 13:38:20 +0800

    add 161

---
+++ ch10/Exercise161.rkt
@@ -0,0 +1,23 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise153) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+(define HOURLY-RATE 14)
+
+; List-of-numbers -> List-of-numbers
+; computes the weekly wages for all given weekly hours
+(check-expect (wage* '()) '())
+(check-expect (wage* (cons 28 '())) (cons (* 28 HOURLY-RATE) '()))
+(check-expect (wage* (cons 4 (cons 2 '())))
+              (cons (* 4 HOURLY-RATE)
+                    (cons (* 2 HOURLY-RATE) '())))
+
+(define (wage* whrs)
+  (cond
+    [(empty? whrs) '()]
+    [else (cons (wage (first whrs)) (wage* (rest whrs)))]))
+ 
+; Number -> Number
+; computes the wage for h hours of work
+(define (wage h)
+  (* HOURLY-RATE h))
