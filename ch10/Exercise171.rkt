e2b580988f54a82c87bd0f613e45ea56223725b7
jesseck3013 <jesseck3013@outlook.com>
2025-07-24 15:56:53 +0800

    add 171

---
+++ ch10/Exercise171.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise153) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; List-of-strings is one of:
+; - '()
+; - (cons String List-of-strings)
+
+(read-lines "./ttt.txt")
+(read-words "./ttt.txt")
+
+; List-of-list-of-strings is one of:
+; - '()
+; - (cons List-of-strings List-of-list-of-string)
+
+(read-words/line "./ttt.txt")
