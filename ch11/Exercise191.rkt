30a047b4f69501de3be6f07cd182eba34935eb20
jesseck3013 <jesseck3013@outlook.com>
2025-07-25 16:39:59 +0800

    add 191

---
+++ ch11/Exercise191.rkt
@@ -0,0 +1,28 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |11|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+(define square-p
+  (list
+   (make-posn 10 10)
+   (make-posn 20 10)
+   (make-posn 20 20)
+   (make-posn 10 20)))
+
+(define triangle-p
+  (list
+   (make-posn 20 10)
+   (make-posn 20 20)
+   (make-posn 30 20)))
+
+(check-expect (connect-dots MT triangle-p)
+              (scene+line
+               (scene+line MT 20 20 30 20 "red")
+               20 10 20 20 "red"))
+
+(check-expect (connect-dots MT square-p)
+              (scene+line 
+               (scene+line
+                (scene+line MT 10 10 20 10 "red")
+                20 10 20 20 "red")
+               20 20 10 20))
