3d7c42537b64a2b05bac71c3b8c5c585c51fdbc2
jesseck3013 <jesseck3013@outlook.com>
2025-07-25 11:40:19 +0800

    add 185

---
+++ ch11/Exercise185.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |11|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+(check-expect 1 (first (list 1 2 3)))
+
+(check-expect (list 2 3) (rest (list 1 2 3)))
+
+(check-expect 2 (second (list 1 2 3)))
+
+(check-expect 3 (third (list 1 2 3)))
+(check-expect 4 (fourth (list 1 2 3 4)))
