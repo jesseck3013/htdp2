22c1773bbc6193f7c772c6ace56e52fdbb95d0d0
jesseck3013 <jesseck3013@outlook.com>
2025-07-25 17:17:02 +0800

    add 11.4

---
+++ ch11/11.4.rkt
@@ -0,0 +1,92 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |11|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+(define square-p
+  (list
+   (make-posn 10 10)
+   (make-posn 20 10)
+   (make-posn 20 20)
+   (make-posn 10 20)))
+
+(define triangle-p
+  (list
+   (make-posn 20 10)
+   (make-posn 20 20)
+   (make-posn 30 20)))
+
+; A Polygon is one of:
+; – (list Posn Posn Posn)
+; – (cons Posn Polygon)
+
+; a plain background image 
+(define MT (empty-scene 50 50))
+
+; Image Posn Posn -> Image 
+; draws a red line from Posn p to Posn q into im
+(check-expect (render-line MT (make-posn 1 1) (make-posn 2 2)) (scene+line MT 1 1 2 2 "red"))
+(define (render-line img p q)
+  (scene+line
+   img
+   (posn-x p) (posn-y p)
+   (posn-x q) (posn-y q)
+  "red"))
+ 
+; Image Polygon -> Image
+; renders the given polygon p into img
+(define (render-poly img p)
+  (render-line 
+   (connect-dots img  p)
+   (first p) (last p)))
+
+; NELoP -> Posn
+; extracts the last item from p
+(define (last p)
+  (cond
+    [(empty? (rest p)) (first p)]
+    [else (last (rest p))]))
+
+(check-expect
+ (render-poly MT triangle-p)
+ (scene+line
+  (scene+line
+   (scene+line MT 20 10 20 20 "red")
+   20 20 30 20 "red")
+  30 20 20 10 "red"))
+
+(check-expect
+ (render-poly MT square-p)
+ (scene+line
+  (scene+line
+   (scene+line
+    (scene+line MT 10 10 20 10 "red")
+    20 10 20 20 "red")
+   20 20 10 20 "red")
+  10 20 10 10 "red"))
+
+; An NELoP is one of: 
+; – (cons Posn '())
+; – (cons Posn NELoP)
+
+; Image NELoP -> Image 
+; connects the dots in p by rendering lines in img
+(define (connect-dots img p)
+  (cond
+    [(empty? (rest p)) img]
+    [else
+     (render-line
+      (connect-dots img (rest p))
+      (first p) (second p))]))
+
+(check-expect (connect-dots MT triangle-p)
+              (scene+line
+               (scene+line MT 20 20 30 20 "red")
+               20 10 20 20 "red"))
+
+(check-expect (connect-dots MT square-p)
+              (scene+line 
+               (scene+line
+                (scene+line MT 10 10 20 10 "red")
+                20 10 20 20 "red")
+               20 20 10 20 "red"))
+
