04b2dd804e8d2715a878fff45b55b55981a81313
jesseck3013 <jesseck3013@outlook.com>
2025-07-27 16:03:36 +0800

    add 211

---
+++ ch12/Exercise211.rkt
@@ -0,0 +1,67 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |12.2|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp")) #f)))
+
+; A Word is one of:
+; – '() or
+; – (cons 1String Word)
+; interpretation a Word is a list of 1Strings (letters)
+
+; String -> Word
+; converts s to the chosen word representation
+(check-expect (string->word "") '())
+(check-expect (string->word "hello")
+              (list "h" "e" "l" "l" "o"))
+(define (string->word s)
+  (cond
+    [(= (string-length s) 0) '()]
+    [else
+     (cons
+      (string-ith s 0)
+      (string->word (substring s 1 (string-length s))))]))
+
+ 
+; Word -> String
+; converts w to a string
+(check-expect (word->string '()) "")
+(check-expect
+ (word->string (list "h" "e" "l" "l" "o")) "hello")
+(define (word->string w)
+  (cond
+    [(empty? w) ""]
+    [else (string-append (first w)
+                         (word->string (rest w)))]))
+
+     
+; List-of-words -> List-of-strings
+(check-expect (words->strings '()) '())
+(check-expect (words->strings
+               (list 
+                (list "h" "e" "l" "l" "o")
+                (list "w" "o" "r" "l" "d")))
+              (list "hello" "world"))
+
+(define (words->strings low)
+  (cond
+    [(empty? low) '()]
+    [else
+     (cons (word->string (first low))
+           (words->strings (rest low)))]))
+
+(define LOCATION "/usr/share/dict/words")
+ 
+; A Dictionary is a List-of-strings.
+(define AS-LIST (read-lines LOCATION))
+
+; List-of-strings -> List-of-strings
+; picks out all those Strings that occur in the dictionary
+(check-expect (in-dictionary '()) '())
+(check-expect (in-dictionary (list "apple" "elppa"))
+             (list "apple"))
+
+(define (in-dictionary los)
+  (cond
+    [(empty? los) '()]
+    [(member? (first los) AS-LIST)
+     (cons (first los) (in-dictionary (rest los)))]
+    [else (in-dictionary (rest los))]))
