900bf073a84f045cbb40984a4b348e75ebb7e1f5
jesseck3013 <jesseck3013@outlook.com>
2025-07-30 13:18:46 +0800

    add 244

---
+++ ch14/Exercise244.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname |14.1|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
+
+(define (f x) (x 10))
+; legal, function is value
+
+(define (f x) (x f))
+; legal, function is value
+
+(define (f x y) (x 'a y 'b))
+; legal, function is value
