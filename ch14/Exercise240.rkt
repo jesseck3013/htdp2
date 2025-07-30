ef105ba2b23c3bf75069b7b43d260eaa2bd760fd
jesseck3013 <jesseck3013@outlook.com>
2025-07-30 12:51:05 +0800

    add 240

---
+++ ch14/Exercise240.rkt
@@ -0,0 +1,32 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname |14.1|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
+
+(define-struct layer [stuff])
+
+; An LStr is one of: 
+; – String
+; – (make-layer LStr)
+"test"
+(make-layer "test")
+(make-layer (make-layer "test"))
+
+; An LNum is one of: 
+; – Number
+; – (make-layer LNum)
+10
+(make-layer 10)
+(make-layer (make-layer 10))
+
+; Abstraction:
+; An LItem is one of:
+; - Item
+; - (make-layer LItem)
+
+; An LStr is an an LItem where Item is String:
+; - String
+; - (make-layer LStr)
+
+; An LNum is an an LItem where Item is Number:
+; - Number
+; - (make-layer LNum)
