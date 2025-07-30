067951dd2441c167dc70ef5dfee93f28a24ca637
jesseck3013 <jesseck3013@outlook.com>
2025-07-30 12:53:40 +0800

    add 241

---
+++ ch14/Exercise241.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname |14.1|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
+
+; An NEList-of-temperatures is one of: 
+; – (cons CTemperature '())
+; – (cons CTemperature NEList-of-temperatures)
+; interpretation non-empty lists of Celsius temperatures
+
+; NEList-of-Booleans is one of:
+; - (cons Boolean '())
+; - (cons Boolean NEList-of-Booleans)
+
+; An NEList-of is one of:
+; (cons ITEM '())
+; (cons ITEM NEList-of)
