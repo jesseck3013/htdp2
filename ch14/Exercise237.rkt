9a109894ad45c44de27213d675b1c8d80994f91e
jesseck3013 <jesseck3013@outlook.com>
2025-07-30 12:19:23 +0800

    add 237

---
+++ ch14/Exercise237.rkt
@@ -0,0 +1,25 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname |14.1|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
+
+(define (extract R l t)
+  (cond
+    [(empty? l) '()]
+    [else (cond
+            [(R (first l) t)
+             (cons (first l)
+                   (extract R (rest l) t))]
+            [else
+             (extract R (rest l) t)])]))
+
+; Number Number -> Boolean
+; is the area of a square with side x larger than c
+(define (squared>? x c)
+  (> (* x x) c))
+
+(squared>? 3 10)
+; output: #false
+(squared>? 4 10)
+; output: #true
+(squared>? 5 10)
+; output: #true
