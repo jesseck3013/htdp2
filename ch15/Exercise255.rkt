f65bef2a36080789cfec8e6fe33657947641bd84
jesseck3013 <jesseck3013@outlook.com>
2025-07-30 16:00:23 +0800

    add 255

---
+++ ch15/Exercise255.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname Exercise246) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
+
+; [List-of Number] [Number -> Number] -> [List-of Number]
+(define (map-n lon func) lon)
+
+; [List-of String] [String -> String] -> [List-of String]
+(define (map-s lon func) lon)
+
+; corresponding part:
+; 1. Number -> String
+
+; [X] [List-of X] [X -> X] -> [List-of X]
+
+; [List-of IR] [IR -> IR] -> [List-of IR]
