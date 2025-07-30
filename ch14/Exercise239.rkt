1ce6da39e986cbecd25fe0fae794c25f899de073
jesseck3013 <jesseck3013@outlook.com>
2025-07-30 12:44:05 +0800

    add 239

---
+++ ch14/Exercise239.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname |14.1|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
+
+; A [List X Y] is a structure: 
+;   (cons X (cons Y '()))
+
+; pairs of Numbers
+; [List Number Number]
+(list 1 2)
+
+; pairs of Number and 1Strings
+; [List Number 1String]
+(list 1 "a")
+
+; pairs of Strings and Booleans
+; [List String Boolean]
+(list "xxx" #false)
