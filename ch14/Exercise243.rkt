6e232f9f4abc0529494c7ca009dd74bec64ff259
jesseck3013 <jesseck3013@outlook.com>
2025-07-30 13:09:35 +0800

    add 243

---
+++ ch14/Exercise243.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname |14.1|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
+
+(define (f x) x)
+
+(cons f '())
+; (list function:f)
+; a list with one item which is f
+
+(f f)
+; function:f
+; a function f because f returns the parameter
+
+(cons f (cons 10 (cons (f 10) '())))
+;(list function:f 10 10)
+; a list whose items are either function f or 10
+
