712c8d7662690c2a2a57ce5bfb27162c422d6dd6
jesseck3013 <jesseck3013@outlook.com>
2025-08-02 10:26:17 +0800

    added 271

---
+++ ch16/Exercise271.rkt
@@ -0,0 +1,29 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise265) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
+
+; String [List-of String] -> Boolean
+(check-expect (find-name "Bob"
+                         (list "Jack" "Henry" "Bob")) #true)
+(check-expect (find-name "Zed"
+                         (list "Jack" "Henry" "Bob")) #false)
+(define (find-name name lon)
+  (local (
+          (define (match item)
+            (string=? name item))
+          )
+    (ormap match lon)))
+
+; [List-of String] -> Boolean
+(check-expect (all-start-with-a (list "apple"
+                                      "andy")) #true)
+(check-expect (all-start-with-a (list "yellow"
+                                      "andy")) #false)
+(define (all-start-with-a lon)
+  (local (
+          (define (match item)
+            (string=? "a" (string-ith item 0)))
+          )
+    (andmap match lon)))
+
+; andmap can ensure all names do not exceed a given width
