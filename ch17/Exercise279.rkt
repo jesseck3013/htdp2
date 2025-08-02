16408024e4b552d7e77af4557c549af046e13c02
jesseck3013 <jesseck3013@outlook.com>
2025-08-02 15:00:46 +0800

    added 279

---
+++ ch17/Exercise279.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise265) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
+
+; (lambda (variable-1 ... variable-N) expression)
+
+(lambda (x y) (x y y))
+
+; (lambda () 10)
+; this one is illegal because there is no variable
+
+(lambda (x) x)
+
+(lambda (x y) x)
+
+;(lambda x 10)
+; this one is illegal because x should be enclosed in parentheses.
+
