57b27c11902a855ec0d43f24c062f83548788639
jesseck3013 <jesseck3013@outlook.com>
2025-08-02 18:26:01 +0800

    added 297

---
+++ ch17/Exercise297.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise265) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
+
+; Number Number Posn -> Number
+(check-expect (distance-between 3 4 (make-posn 0 0)) 5)
+(define (distance-between x y p)
+  (sqrt (+ (sqr (- x (posn-x p)))
+           (sqr (- y (posn-y p))))))
