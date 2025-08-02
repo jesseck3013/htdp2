101b1803d579877372d3b914d8d517b691f3051d
jesseck3013 <jesseck3013@outlook.com>
2025-08-02 15:21:50 +0800

    added 283

---
+++ ch17/Exercise283.rkt
@@ -0,0 +1,19 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise283) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
+
+(map (lambda (x) (* 10 x))
+     '(1 2 3))
+
+(foldl (lambda (name rst)
+         (string-append name ", " rst))
+       "etc."
+       '("Matthew" "Robby"))
+
+(define-struct ir [name price])
+(define th 100)
+(filter (lambda (ir) (<= (ir-price ir) th))
+        (list (make-ir "bear" 10)
+              (make-ir "doll" 33)))
+
+''
