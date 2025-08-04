9a70a007150afd577072fda536d524a16eaea13d
jesseck3013 <jesseck3013@outlook.com>
2025-08-04 13:59:10 +0800

    added 313

---
+++ ch19/Exercise313.rkt
@@ -0,0 +1,74 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise298) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
+
+(define-struct child [father mother name date eyes])
+
+; A Child is a structure: 
+;   (make-child Child Child String N String)
+
+(define-struct no-parent [])
+
+; An FT (short for family tree) is one of: 
+; – (make-no-parent)
+; – (make-child FT FT String N String)
+
+(define NP (make-no-parent))
+; An FT is one of: 
+; – NP
+; – (make-child FT FT String N String)
+
+; Oldest Generation:
+(define Carl (make-child NP NP "Carl" 1926 "green"))
+(define Bettina (make-child NP NP "Bettina" 1926 "green"))
+ 
+; Middle Generation:
+(define Adam (make-child Carl Bettina "Adam" 1950 "hazel"))
+(define Dave (make-child Carl Bettina "Dave" 1955 "black"))
+(define Eva (make-child Carl Bettina "Eva" 1965 "blue"))
+(define Fred (make-child NP NP "Fred" 1966 "pink"))
+ 
+; Youngest Generation: 
+(define Gustav (make-child Fred Eva "Gustav" 1988 "brown"))
+
+; FT -> Boolean
+; does an-ftree contain a child
+; structure with "blue" in the eyes field
+ 
+(check-expect (blue-eyed-child? Carl) #false)
+(check-expect (blue-eyed-child? Gustav) #true)
+ 
+(define (blue-eyed-child? an-ftree)
+  (cond
+    [(no-parent? an-ftree) #false]
+    [else (or (string=? (child-eyes an-ftree) "blue")
+              (blue-eyed-child? (child-father an-ftree))
+              (blue-eyed-child? (child-mother an-ftree)))]))
+
+; FT -> Boolean
+; Does any of ancestors of the given child has blue eyes
+(check-expect (blue-eyed-ancestor? Eva) #false)
+(check-expect (blue-eyed-ancestor? Gustav) #true)
+(define (blue-eyed-ancestor? an-ftree)
+  (cond
+    [(no-parent? an-ftree) #false]
+    [else
+     (or
+       (blue-eyed-child?
+         (child-father an-ftree))
+       (blue-eyed-child?
+         (child-mother an-ftree)))]))
+
+; Original solution:
+;; (define (blue-eyed-ancestor? an-ftree)
+;;   (cond
+;;     [(no-parent? an-ftree) #false]
+;;     [else
+;;      (or
+;;        (blue-eyed-ancestor?
+;;          (child-father an-ftree))
+;;        (blue-eyed-ancestor?
+;;          (child-mother an-ftree)))]))
+
+; This function fails because it always return #false.
+; To fix the issue, I replaced the recusive call blue-eyed-ancestor? with blue-eyed-child?
