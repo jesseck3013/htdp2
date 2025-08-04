514869ff72f2ed274cdd4e78e4ab72cb8bee8bc5
jesseck3013 <jesseck3013@outlook.com>
2025-08-04 17:43:27 +0800

    added 331

---
+++ ch20/Exercise331.rkt
@@ -0,0 +1,28 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise298) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
+
+; A Dir.v1 (short for directory) is one of: 
+; – '()
+; – (cons File.v1 Dir.v1)
+; – (cons Dir.v1 Dir.v1)
+ 
+; A File.v1 is a String.
+
+(define ts (list "read!"
+                 (list "part1"
+                       "part2"
+                       "part3")
+                 (list
+                  (list "hang"
+                        "draw")
+                  (list "read!"))))
+
+; Dir.v1 -> Number
+(check-expect (how-many ts) 7)
+(define (how-many ts)
+  (cond
+    [(empty? ts) 0]
+    [(string? (first ts)) (add1 (how-many (rest ts)))]
+    [else (+ (how-many (first ts))
+             (how-many (rest ts)))]))
