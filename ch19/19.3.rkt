3a184427c9351439e56dfab794aa96e994242785
jesseck3013 <jesseck3013@outlook.com>
2025-08-04 15:56:53 +0800

    added 19.3

---
+++ ch19/19.3.rkt
@@ -0,0 +1,58 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise298) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
+
+; An S-expr is one of: 
+; – Atom
+; – SL
+
+; An Atom is one of: 
+; – Number
+; – String
+; – Symbol
+
+; An SL is one of: 
+; – '()
+; – (cons S-expr SL)
+
+; Any -> Boolean
+; Output true if the type of expr is one of
+; - number
+; - string
+; - symbol
+(check-expect (atom? 1) #true)
+(check-expect (atom? "hello") #true)
+(check-expect (atom? 'xx) #true)
+(check-expect (atom? (list 1 2 3)) #false)
+(define (atom? expr)
+  (cond
+    [(number? expr) #true]
+    [(string? expr) #true]
+    [(symbol? expr) #true]
+    [else #false]))
+
+; S-expr Symbol -> N 
+; counts all occurrences of sy in sexp 
+(check-expect (count 'world 'hello) 0)
+(check-expect (count '(world hello) 'hello) 1)
+(check-expect (count '(((world) hello) hello) 'hello) 2)
+
+(define (count sexp sy)
+ (cond
+   [(atom? sexp) (count-atom sexp sy)]
+   [else (count-sl sexp sy)]))
+
+(define (count-atom at sy)
+  (cond
+    [(number? at) 0]
+    [(string? at) 0]
+    [(symbol? at) (if (equal? at sy)
+                      1
+                      0)]))
+
+(define (count-sl sl sy)
+  (cond
+    [(empty? sl) 0]
+    [else
+     (+ (count (first sl) sy)
+        (count-sl (rest sl) sy))]))
