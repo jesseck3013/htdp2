89f21a0cb8dd9ac70ae6a9511573b40435efada7
jesseck3013 <jesseck3013@outlook.com>
2025-08-04 16:04:18 +0800

    added 323

---
+++ ch19/Exercise323.rkt
@@ -0,0 +1,53 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise298) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
+
+(define-struct no-info [])
+(define NONE (make-no-info))
+ 
+(define-struct node [ssn name left right])
+; A BT (short for BinaryTree) is one of:
+; – NONE
+; – (make-node Number Symbol BT BT)
+
+;  15
+; /  \
+;     24'i
+
+;  15 'd
+; /  \
+;87'h
+
+(define bt1 (make-node
+             15
+             'd
+             NONE
+             (make-node
+              24 'i NONE NONE)))
+
+(define bt2 (make-node
+             15
+             'd
+             (make-node
+              87 'h NONE NONE)
+             NONE))
+
+; BT Number -> Symbol
+(check-expect (search NONE 10) #false)
+(check-expect (search bt1 30) #false)
+(check-expect (search bt1 24) 'i)
+(define (search bt n)
+  (cond
+    [(no-info? bt) #false]
+    [else
+     (local (
+             (define left-result
+               (search (node-left bt) n))
+             (define right-result
+               (search (node-right bt) n))
+             )
+     (cond
+       [(equal? (node-ssn bt) n) (node-name bt)]
+       [(boolean? left-result) right-result]
+       [else left-result]))]))
+
