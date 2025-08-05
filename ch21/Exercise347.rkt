578da0ef03fc975996f2c47a3b423eaff51a01ba
jesseck3013 <jesseck3013@outlook.com>
2025-08-05 15:17:49 +0800

    added 347

---
+++ ch21/Exercise347.rkt
@@ -0,0 +1,27 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |20.3|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+(define-struct add [left right])
+(define-struct mul [left right])
+
+; A BSL-Expr (BSL Expression), is one of:
+; - Number
+; - (make-add BSL-Expr BSL-Expr)
+; - (make-mul BSL-Expr BSL-Expr)
+
+; BSL-Expr -> Number
+; Evaluate the given BSL-Expr to a number.
+(check-expect (eval-expression 10) 10)
+(check-expect (eval-expression (make-add 1 1)) 2)
+(check-expect (eval-expression (make-mul 2 3)) 6)
+(check-expect (eval-expression
+               (make-add 1 (make-mul 2 3))) 7)
+
+(define (eval-expression expr)
+  (cond
+    [(number? expr) expr]
+    [(add? expr) (+ (eval-expression (add-left expr))
+                    (eval-expression (add-right expr)))]
+    [(mul? expr) (* (eval-expression (mul-left expr))
+                    (eval-expression (mul-right expr)))]))
