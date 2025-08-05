7e38c1dcabfb3c5f213c22ebcfd75af9991e50f4
jesseck3013 <jesseck3013@outlook.com>
2025-08-06 05:21:24 +0800

    added 365

---
+++ ch22/Exercise365.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise349) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+'(server ((name "example.org")))
+; <server name="example.org"></server>
+
+'(carcas (board (grass)) (player ((name "sam"))))
+; <carcas>
+; <board><grass /></board>
+; <player name="sam"></player>
+
+'(start)
+; <start />
+; This is Xexpr.v0 or Xexpr.v1
