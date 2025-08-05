11afbd0d9342de7afe76d97455aff9099ec50789
jesseck3013 <jesseck3013@outlook.com>
2025-08-05 15:46:09 +0800

    added 350

---
+++ ch21/Exercise350.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise349) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+(define WRONG "Error: Syntax Incorrect")
+
+; parse-sl is different from the design recipe.
+; What parse-sl accepts is a list.
+; There are two cond clauses when applying the design recipe
+; to a list processing function.
+;; (cond
+;;   [(empty? s)...]
+;;   [else ...(first s)...(rest s)...])
