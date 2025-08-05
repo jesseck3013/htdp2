4f5e436e2e99e42f2d587b784a6d64a03ad605ee
jesseck3013 <jesseck3013@outlook.com>
2025-08-05 15:13:05 +0800

    added 346

---
+++ ch21/Exercise346.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |20.3|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+(define-struct add [left right])
+(define-struct mul [left right])
+
+; A BSL-Expr (BSL Expression), is one of:
+; - Number
+; - (make-add BSL-Expr BSL-Expr)
+; - (make-mul BSL-Expr BSL-Expr)
