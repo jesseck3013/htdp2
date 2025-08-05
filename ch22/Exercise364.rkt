b1cdf88c594f2e51b2450b0cc4045f33778eac8d
jesseck3013 <jesseck3013@outlook.com>
2025-08-06 05:21:19 +0800

    added 364

---
+++ ch22/Exercise364.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise349) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; <transition from="seen-e" to="seen-f" />
+'(transition ((from "seen-e")
+              (to "seef-f")))
+
+; <ul><li><word /><word /></li><li><word /></li></ul>
+'(ul
+  (li
+   (word)
+   (word))
+  (li
+   (word)))
+
+; This can be represented by Xexpr.v1.
