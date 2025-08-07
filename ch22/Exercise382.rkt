a6d7df1cdb3d157753ffe670b016015153d84491
jesseck3013 <jesseck3013@outlook.com>
2025-08-07 10:19:14 +0800

    added 382

---
+++ ch22/Exercise382.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |22.2|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; XML Configuration:
+
+;; <machine initial="black">
+;; <action state="black" next="white" />
+;; <action state="white" next="black" />
+;; </machine>
+
+
+; XMachine representation:
+
+'(machine ((initial "black"))
+          (action ((state "black") (next "white")))
+          (action ((state "white") (next "black"))))
