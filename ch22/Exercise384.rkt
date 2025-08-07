c5e86ad3f0df3d89c1335b5fccd90885b6a4c3dd
jesseck3013 <jesseck3013@outlook.com>
2025-08-07 13:07:03 +0800

    added 384

---
+++ ch22/Exercise384.rkt
@@ -0,0 +1,47 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |22.2|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; The difference between read-xexpr/web and read-plain-xexpr/web:
+
+; read-plain-xexpr/web: if not 404, the output is the first XML element without **whitespace**
+
+; read-xexpr/web: if not 404 , the output includes **whitespace**.
+
+(define PREFIX "Https://www.google.com/finance?q=")
+(define SIZE 22) ; font size 
+ 
+(define-struct data [price delta])
+; A StockWorld is a structure: (make-data String String)
+ 
+; String -> StockWorld
+; retrieves the stock price of co and its change every 15s
+(define (stock-alert co)
+  (local ((define url (string-append PREFIX co))
+          ; [StockWorld -> StockWorld]
+          ;; 1. Get the current stock price via a web request
+          ;; to url.
+          ;; 2. Compute the price change based on the price
+          ;; of the previous request.
+          (define (retrieve-stock-data __w)
+            (local ((define x (read-xexpr/web url)))
+              (make-data (get x "price")
+                         (get x "priceChange"))))
+          ; StockWorld -> Image
+          ;; Given a StockWorld,
+          ;; draw the current price and the price change
+          (define (render-stock-data w)
+            (local (; [StockWorld String -> String] -> Image
+                    ;; Render the word sel with the color 
+                    (define (word sel col)
+                      (text (sel w) SIZE col)))
+              ;; render price on the left with black color
+              ;; render price change on the right with red color
+              ;; put the words into a white box
+              (overlay (beside (word data-price 'black)
+                               (text "  " SIZE 'white)
+                               (word data-delta 'red))
+                       (rectangle 300 35 'solid 'white)))))
+    (big-bang (retrieve-stock-data 'no-use)
+      [on-tick retrieve-stock-data 15]
+      [to-draw render-stock-data])))
