f46f748823168aa643a980dc5d60aafa0b22fb41
jesseck3013 <jesseck3013@outlook.com>
2025-08-08 13:18:26 +0800

    added 408

---
+++ ch23/Exercise408.rkt
@@ -0,0 +1,120 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |22.2|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+(define-struct db [schema content])
+; A DB is a structure: (make-db Schema Content)
+
+; A Schema is a [List-of Spec]
+; A Spec is a [List Label Predicate]
+; A Label is a String
+; A Predicate is a [Any -> Boolean]
+
+; A (piece of) Content is a [List-of Row]
+; A Row is a [List-of Cell]
+; A Cell is Any
+; constraint cells do not contain functions 
+
+; integrity constraint In (make-db sch con), 
+; for every row in con,
+; (I1) its length is the same as sch's, and
+; (I2) its ith Cell satisfies the ith Predicate in sch
+
+(define schema1 `(("Name" ,string?)
+                  ("Age" ,integer?)
+                  ("Present" ,boolean?)))
+
+(define schema2 `(("Present" ,boolean?)
+                  ("Description" ,string?)))
+
+(define s1r1 '("Alice" 35 #true))
+(define s1r2 '("Bob" 25 #false))
+(define s1r3 '("Carol" 30 #true))
+(define s1r4 '("Dave" 32 #false))
+
+(define s2r1 '(#true "presence"))
+(define s2r2 '(#false "absence"))
+
+(define db1 (make-db schema1 (list s1r1
+                                   s1r2
+                                   s1r3
+                                   s1r4)))
+
+(define db2 (make-db schema2 (list s2r1
+                                   s2r2)))
+
+(define (integrity-check db)
+  (local (; Row -> Boolean 
+          ; does row satisfy (I1) and (I2) 
+          (define (row-integrity-check row)
+            (and (= (length row)
+                    (length (db-schema db)))
+                 (andmap (lambda (s c) [(second s) c])
+                         (db-schema db)
+                         row))))
+    (andmap row-integrity-check (db-content db))))
+
+(check-expect
+ (integrity-check (make-db (list
+                            (list "name" string?))
+                           (list (list 1))))
+ #false)
+
+(define projected-content
+  `(("Alice" #true)
+    ("Bob"   #false)
+    ("Carol" #true)
+    ("Dave"  #false)))
+
+(define projected-schema
+  `(("Name" ,string?) ("Present" ,boolean?)))
+
+(define projected-db
+  (make-db projected-schema projected-content))
+
+(check-expect
+ (db-content (project db1 '("Name" "Present")))
+ projected-content)
+
+(define (project db labels)
+  (local ((define schema  (db-schema db))
+          (define content (db-content db))
+ 
+          ; Spec -> Boolean
+          ; does this column belong to the new schema
+          (define (keep? c)
+            (member? (first c) labels))
+ 
+          ; Row -> Row 
+          ; retains those columns whose name is in labels
+          (define (row-project row)
+            (foldr (lambda (cell m c) (if m (cons cell c) c))
+                   '()
+                   row
+                   mask))
+          (define mask (map keep? schema)))
+    (make-db (filter keep? schema)
+             (map row-project content))))
+
+; DB [List-of labels] [Row -> Boolean] -> [List-of Rows]
+(check-expect
+ (select db1
+         (list "Name" "Age")
+         (lambda (row)
+           (>= (second row) 30)))
+ (list
+  '("Alice" 35)
+  '("Carol" 30)
+ '("Dave" 32)))
+
+(define (select db labels predicate)
+  (local (
+          (define schema (db-schema db))
+          (define content (db-content db))
+          (define filtered-content
+            (filter predicate content))
+          (define filtered-db
+            (make-db schema filtered-content))
+          (define projected-db (project filtered-db labels))
+          )
+    (db-content projected-db)))
