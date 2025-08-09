e508fb0504bfa50502cb554973f6fc493a2217dd
jesseck3013 <jesseck3013@outlook.com>
2025-08-10 01:11:26 +0800

    added 438

---
+++ ch26/Exercise438.rkt
@@ -0,0 +1,19 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |25.1|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+(define (gcd-structural n m)
+  (local (; N -> N
+          ; determines the gcd of n and m less than i
+          (define (greatest-divisor-<= i)
+            (cond
+              [(= i 1) 1]
+              [else
+               (if (= (remainder n i) (remainder m i) 0)
+                   i
+                   (greatest-divisor-<= (- i 1)))])))
+    (greatest-divisor-<= (min n m))))
+
+; Starting from the smaller number from n and m, the function checks if this number is GCD between n and m. If it is not the GCD, the function recursively subtracts the number by 1 and check it is a GCD again, until the number becomes 1.
+
+; Because any number that is larger than (min n m) is impossible to be their GCD.
