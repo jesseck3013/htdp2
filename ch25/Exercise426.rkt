bbe2ab85e2f2fe603f80d31b518609aedc4a6122
jesseck3013 <jesseck3013@outlook.com>
2025-08-09 17:30:57 +0800

    added 426

---
+++ ch25/Exercise426.rkt
@@ -0,0 +1,62 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise426) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; [List-of Number] -> [List-of Number]
+; produces a sorted version of alon
+; assume the numbers are all distinct 
+(define (quick-sort< alon)
+  (cond
+    [(empty? alon) '()]
+    [(empty? (rest alon)) alon]
+    [else (local ((define pivot (first alon)))
+            (append (quick-sort< (smallers alon pivot))
+                    (list pivot)
+                    (quick-sort< (largers alon pivot))))]))
+
+; [List-of Number] Number -> [List-of Number]
+; Create a list where all elements are from alon, and
+; all elements are larger than n
+(define (largers alon n)
+  (cond
+    [(empty? alon) '()]
+    [else (if (> (first alon) n)
+              (cons (first alon) (largers (rest alon) n))
+              (largers (rest alon) n))]))
+
+; [List-of Number] Number -> [List-of Number]
+; Create a list where all elements are from alon, and
+; all elements are smaller than n
+(define (smallers alon n)
+  (cond
+    [(empty? alon) '()]
+    [else (if (< (first alon) n)
+              (cons (first alon) (smallers (rest alon) n))
+              (smallers (rest alon) n))]))
+
+; Original quick sort on one item list:
+
+(quick-sort< '(1))
+
+; 1. ---
+(append (quick-sort< '())
+        '(1)
+        (quick-sort< '()))
+
+; 2. ---
+(append '()
+        '(1)
+        '())
+
+; 3. ---
+(list 1)
+
+; Optimized:
+
+(quick-sort< '(1))
+
+; 1. ---
+'(1)
+
+; It saves two steps for every recursive call on a one item list.
+
