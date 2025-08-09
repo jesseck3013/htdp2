8638c9bcb4052b4a5fac794bd5df30e80d6a767c
jesseck3013 <jesseck3013@outlook.com>
2025-08-10 01:36:36 +0800

    added 440

---
+++ ch26/Exercise440.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |25.1|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+(define (gcd-generative n m)
+  (local (; N[>= 1] N -> N
+          ; generative recursion
+          ; (gcd L S) == (gcd S (remainder L S)) 
+          (define (clever-gcd L S)
+            (cond
+              [(= S 0) L]
+              [else (clever-gcd S (remainder L S))])))
+    (clever-gcd (max m n) (min m n))))
+
+(time (gcd-generative 101135853 45014640))
+;; Output:
+;; cpu time: 0 real time: 0 gc time: 0
+;; 177
