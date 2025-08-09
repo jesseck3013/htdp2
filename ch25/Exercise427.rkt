3ca1913fef3aaa91ae1b51684f189f44c26a1b72
jesseck3013 <jesseck3013@outlook.com>
2025-08-09 17:31:02 +0800

    added 427

---
+++ ch25/Exercise427.rkt
@@ -0,0 +1,60 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise426) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+(define THRESHOLD 1000)
+
+; List-of-numbers -> List-of-numbers
+; produces a sorted version of l
+(define (sort> l)
+  (cond
+    [(empty? l) '()]
+    [(cons? l) (insert (first l) (sort> (rest l)))]))
+ 
+; Number List-of-numbers -> List-of-numbers
+; inserts n into the sorted list of numbers l 
+(define (insert n l)
+  (cond
+    [(empty? l) (cons n '())]
+    [else (if (>= n (first l))
+              (cons n l)
+              (cons (first l) (insert n (rest l))))]))
+
+(define (sort< l)
+  (reverse (sort> l)))
+
+; [List-of Number] -> [List-of Number]
+; produces a sorted version of alon
+; assume the numbers are all distinct 
+(define (quick-sort< alon)
+  (if (< (length alon) THRESHOLD)
+      (sort< alon)
+      (cond
+        [(empty? alon) '()]
+        [(empty? (rest alon)) alon]
+        [else (local ((define pivot (first alon)))
+                (append (quick-sort< (smallers alon pivot))
+                        (list pivot)
+                        (quick-sort< (largers alon pivot))))])))
+
+; [List-of Number] Number -> [List-of Number]
+; Create a list where all elements are from alon, and
+; all elements are larger than n
+(define (largers alon n)
+  (cond
+    [(empty? alon) '()]
+    [else (if (> (first alon) n)
+              (cons (first alon) (largers (rest alon) n))
+              (largers (rest alon) n))]))
+
+; [List-of Number] Number -> [List-of Number]
+; Create a list where all elements are from alon, and
+; all elements are smaller than n
+(define (smallers alon n)
+  (cond
+    [(empty? alon) '()]
+    [else (if (< (first alon) n)
+              (cons (first alon) (smallers (rest alon) n))
+              (smallers (rest alon) n))]))
+
+;(quick-sort< '(4 3 2 1))
