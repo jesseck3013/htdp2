be3c4dc0a208d38033d7700ef3c00713084798c4
jesseck3013 <jesseck3013@outlook.com>
2025-08-09 14:18:31 +0800

    added 421

---
+++ ch25/Exercise421.rkt
@@ -0,0 +1,39 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |25.1|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+(check-expect (bundle (explode "abcdefg") 3)
+              (list "abc" "def" "g"))
+
+(check-expect (bundle '("a" "b") 3) (list "ab"))
+
+(check-expect (bundle '() 3) '())
+
+; [List-of 1String] N -> [List-of String]
+; bundles chunks of s into strings of length n
+; idea take n items and drop n at a time
+(define (bundle s n)
+  (cond
+    [(empty? s) '()]
+    [else
+     (cons (implode (take s n)) (bundle (drop s n) n))]))
+ 
+; [List-of X] N -> [List-of X]
+; keeps the first n items from l if possible or everything
+(define (take l n)
+  (cond
+    [(zero? n) '()]
+    [(empty? l) '()]
+    [else (cons (first l) (take (rest l) (sub1 n)))]))
+ 
+; [List-of X] N -> [List-of X]
+; removes the first n items from l if possible or everything
+(define (drop l n)
+  (cond
+    [(zero? n) l]
+    [(empty? l) l]
+    [else (drop (rest l) (sub1 n))]))
+
+; (bundle '("a" "b" "c") 0)
+; This expression is non-stop. Because it does not drop any
+; items in s.
