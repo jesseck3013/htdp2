893054be9bf590b56bbe5271cf04266641acfab3
jesseck3013 <jesseck3013@outlook.com>
2025-08-10 15:36:30 +0800

    added 445

---
+++ ch27/Exercise445.rkt
@@ -0,0 +1,25 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |27.1|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; Number -> Number
+(define (poly x)
+  (* (- x 2) (- x 4)))
+
+; [Number -> Number] Number Number -> Number
+; determines R such that f has a root in [R,(+ R ε)]
+; assume f is continuous 
+; (2) (or (<= (f left) 0 (f right)) (<= (f right) 0 (f left)))
+; generative divides interval in half, the root is in 
+; one of the two halves, picks according to (2)
+(define (find-root f left right)
+  0)
+
+(check-within (find-root poly 0 3)
+              2
+              0.01)
+(check-within (find-root poly 3 5)
+              4
+              0.01)
+
+; n = 2, mid = 3.75, (f mid) = -0.4375
