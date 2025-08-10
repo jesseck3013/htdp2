acd620b3fa5d0505a17de128187c0f6ec50cdb02
jesseck3013 <jesseck3013@outlook.com>
2025-08-10 15:50:34 +0800

    added 450

---
+++ ch27/Exercise450.rkt
@@ -0,0 +1,34 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |27.1|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+(define ε 0.01)
+
+; Number -> Number
+(define (poly x)
+  (* (- x 2) (- x 4)))
+
+; [Number -> Number] Number Number -> Number
+; determines R such that f has a root in [R,(+ R ε)]
+; assume f is continuous 
+; assume (or (<= (f left) 0 (f right)) (<= (f right) 0 (f left)))
+; generative divides interval in half, the root is in one of the two
+; halves, picks according to assumption 
+(define (find-root f left right)
+  (cond
+    [(<= (- right left) ε) left]
+    [else
+      (local ((define mid (/ (+ left right) 2))
+              (define f@mid (f mid))
+              (define f-left (f left))
+              (define f-right (f right)))
+        (cond
+          [(<= f@mid 0)
+           (find-root f mid right)]
+          [(>= f@mid 0)
+           (find-root f left mid)]))]))
+
+(check-within (find-root poly 3 5)
+              4
+              0.01)
+
