7316532a965dc238bd4e3d19f7f530da150c59df
jesseck3013 <jesseck3013@outlook.com>
2025-08-10 15:36:47 +0800

    added 448

---
+++ ch27/Exercise448.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |27.1|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; If a function f is continuous, and there exists an interval
+; [a, b] where (< (f a) 0 (f b)) or (< (f b) 0 (f a)), the function can definitely terminate. Because every recursive call make the interval smaller, eventually it will be smaller than ε.
