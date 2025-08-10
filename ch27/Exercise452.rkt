94f11a21d539f1c8c1ed9862cf77bf7b81a9d5b0
jesseck3013 <jesseck3013@outlook.com>
2025-08-10 19:55:52 +0800

    added 452

---
+++ ch27/Exercise452.rkt
@@ -0,0 +1,30 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |27.1|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; File -> [List-of Line]
+; converts a file into a list of lines 
+(define (file->list-of-lines afile)
+  (cond
+    [(empty? afile) '()]
+    [else
+     (cons (first-line afile)
+           (file->list-of-lines (remove-first-line afile)))]))
+ 
+; File -> Line
+; retrieves the prefix of afile up to the first occurrence of NEWLINE
+(define (first-line afile)
+  (cond
+    [(empty? afile) '()]
+    [(string=? (first afile) NEWLINE) '()]
+    [else (cons (first afile) (first-line (rest afile)))]))
+ 
+; File -> File
+; drops the prefix of afile up to the first occurrence of NEWLINE
+(define (remove-first-line afile)
+  (cond
+    [(empty? afile) '()]
+    [(string=? (first afile) NEWLINE) (rest afile)]
+    [else (remove-first-line (rest afile))]))
+ 
+(define NEWLINE "\n") ; the 1String 
