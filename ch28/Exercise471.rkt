9a86a859e6fea0598294d1ca84bad0b4af55a534
jesseck3013 <jesseck3013@outlook.com>
2025-08-11 17:45:12 +0800

    added 471

---
+++ ch28/Exercise471.rkt
@@ -0,0 +1,26 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise455) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; A Node is a Symbol.
+
+; A Graph is [List-of [List-of Node]]
+
+(define (neighbors n g)
+  (local ((define filtered-list 
+            (filter (lambda (node)
+                      (equal? (first node) n)) g))
+          )
+    (rest (first filtered-list))))
+
+(define sample-graph
+  '((A B E)
+    (B E F)
+    (C D)
+    (D)
+    (E C F)
+    (F D G)
+    (G)))
+
+(check-expect (neighbors 'A sample-graph) '(B E))
+(check-expect (neighbors 'G sample-graph) '())
