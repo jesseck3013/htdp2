e3307b23a1447e887f9fde36760dcd7dc78b62ae
jesseck3013 <jesseck3013@outlook.com>
2025-08-12 10:46:56 +0800

    added 477

---
+++ ch28/Exercise477.rkt
@@ -0,0 +1,34 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise455) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; [List-of X] -> [List-of [List-of X]]
+; creates a list of all rearrangements of the items in w
+(define (arrangements w)
+  (cond
+    [(empty? w) '(())]
+    [else
+      (foldr (lambda (item others)
+               (local ((define without-item
+                         (arrangements (remove item w)))
+                       (define add-item-to-front
+                         (map (lambda (a) (cons item a))
+                              without-item)))
+                 (append add-item-to-front others)))
+        '()
+        w)]))
+ 
+; [List-of [List-of 1String]] -> Boolean
+; are the words "rat", "art", and "tar" members of the given list?
+(define (all-words-from-rat? w)
+  (and (member (explode "rat") w)
+       (member (explode "art") w)
+       (member (explode "tar") w)))
+ 
+(check-satisfied (arrangements '("r" "a" "t"))
+                 all-words-from-rat?)
+
+; 1. '() is the trivial problem
+; 2. the solution to the trivial problem is '() because no arrangement needed.
+; 3. sub-problem: insert a 1string into the front of all arrangements without the 1string.
+; 4. use append to combine all the solutions to the sub-problems
