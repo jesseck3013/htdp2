a17ef7874cef5df16a72fb946284aa1c501ad618
jesseck3013 <jesseck3013@outlook.com>
2025-08-12 16:12:49 +0800

    added 489

---
+++ ch31/Exercise489.rkt
@@ -0,0 +1,29 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise489) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; [List-of Number] -> [List-of Number]
+; converts a list of relative to absolute distances
+; the first number represents the distance to the origin
+ 
+(check-expect (relative->absolute '(50 40 70 30 30))
+              '(50 90 160 190 220))
+ 
+(define (relative->absolute l)
+  (cond
+    [(empty? l) '()]
+    [else (local ((define rest-of-l
+                    (relative->absolute (rest l)))
+                  (define adjusted
+                    (add-to-each (first l) rest-of-l)))
+            (cons (first l) adjusted))]))
+ 
+; Number [List-of Number] -> [List-of Number]
+; adds n to each number on l
+ 
+(check-expect (cons 50 (add-to-each 50 '(40 110 140 170)))
+              '(50 90 160 190 220))
+ 
+(define (add-to-each n l)
+  (map (lambda (item)
+         (+ n item)) l))
