a13c7a7560270cc0a55cdf7251ce717971a2f9c9
jesseck3013 <jesseck3013@outlook.com>
2025-08-12 11:21:34 +0800

    added 478

---
+++ ch28/Exercise478.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise455) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; Placing the first queen in all squares of the top-most row, the right-most column is the same operation.
+
+; We can rotate the chess board.
+; Clockwise 90 degrees, top -> right.
+; Clockwise 90 degrees, right -> left.
+
+; Based on this observation, we can simplify the question to placing the first queen in all 3 squares of the top-most row.
+
+; It it impossible.
