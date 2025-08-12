e9fddd0d0bb57f95ded142eb512f74853bd897e0
jesseck3013 <jesseck3013@outlook.com>
2025-08-12 23:48:44 +0800

    added 491

---
+++ ch31/Exercise491.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise489) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+(define (relative->absolute l)
+ (reverse
+   (foldr (lambda (element accu) (cons (+ element (first accu)) accu))
+          (list (first l))
+          (reverse (rest l)))))
+
+; This implementation also uses the accumulator's idea. In addition to the accumulator, this implementation also introduces reverse twice. It actually slows down the performance compared to the original one.
+
