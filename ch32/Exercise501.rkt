12d8898156d1121817f9dd9ccf166dffc10eaece
jesseck3013 <jesseck3013@outlook.com>
2025-08-13 12:39:51 +0800

    added 501

---
+++ ch32/Exercise501.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise489) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; N -> Number 
+; adds n to pi without using +
+(check-within (add-to-pi 2) (+ 2 pi) 0.001)
+(define (add-to-pi n)
+  (local (
+          ; acc is the summation of the numbers
+          ; subtracted from n and pi.
+          (define (add-to-pi/acc n acc)
+           (cond
+             [(zero? n) acc]
+             [else (add-to-pi/acc (sub1 n)
+                                  (add1 acc))])))
+    (add-to-pi/acc n pi)))
+
