cbb2d6c4b95fdbabe8654ef2aafb53e1c58d6fdc
jesseck3013 <jesseck3013@outlook.com>
2025-08-13 12:23:54 +0800

    added 499

---
+++ ch32/Exercise499.rkt
@@ -0,0 +1,19 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise489) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; [List-of Number] -> Number
+(define (product lon)
+  (local (
+          (define (product/acc lon acc)
+            (cond
+              [(empty? lon) acc]
+              [else (product/acc (rest lon)
+                                 (* (first lon) acc))]))
+          )
+    (product/acc lon 1)))
+
+(check-expect (product '(2)) 2)
+(check-expect (product '(1 2 3 4)) 24)
+
+; The accumulator style is also O(n).
