b5cacd6b4d049c0bf3cf34d41518aefa4fd6811e
jesseck3013 <jesseck3013@outlook.com>
2025-08-13 12:35:49 +0800

    added comments in 499 and 500

--- ch32/Exercise499.rkt
+++ ch32/Exercise499.rkt
@@ -3,15 +3,17 @@
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise489) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
 
 ; [List-of Number] -> Number
-(define (product lon)
+(define (product l0)
   (local (
+          ; acc is the prodcut of all items that
+          ; are removed from lon.
           (define (product/acc lon acc)
             (cond
               [(empty? lon) acc]
               [else (product/acc (rest lon)
                                  (* (first lon) acc))]))
           )
-    (product/acc lon 1)))
+    (product/acc l0 1)))
 
 (check-expect (product '(2)) 2)
 (check-expect (product '(1 2 3 4)) 24)
