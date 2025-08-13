dd22ebf26cad517fb8ad0c8df5ace0e46f96f3e0
jesseck3013 <jesseck3013@outlook.com>
2025-08-13 16:21:28 +0800

    added 508

---
+++ ch32/Exercise508.rkt
@@ -0,0 +1,90 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise489) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+(define FONT-SIZE 11)
+(define FONT-COLOR "black")
+ 
+; [List-of 1String] -> Image
+; renders a string as an image for the editor 
+(define (editor-text s)
+  (text (implode s) FONT-SIZE FONT-COLOR))
+ 
+(define-struct editor [pre post])
+; An Editor is a structure:
+;   (make-editor [List-of 1String] [List-of 1String])
+; interpretation if (make-editor p s) is the state of 
+; an interactive editor, (reverse p) corresponds to
+; the text to the left of the cursor and s to the
+; text on the right
+
+; [X] X [List-of X] -> [List-of X]
+(define (add-to-last x lox)
+  (cond
+    [(empty? lox) (list x)]
+    [else (cons (first lox)
+                (add-to-last x (rest lox)))]))
+
+; [List-of 1String] N -> Editor
+(define (split-structural ed x)
+  (cond
+    [(empty? ed) (make-editor '() '())]
+    [else
+     (local (
+             (define current-ed (split-structural
+                                 (rest ed)
+                                 x))
+             (define current-pre (editor-pre current-ed))
+             (define current-post (editor-post current-ed))
+             (define one-more-on-pre (add-to-last
+                                      (first ed)
+                                      current-pre))
+             )
+       (if (>=
+            x
+            (image-width (editor-text one-more-on-pre)))
+            (make-editor one-more-on-pre current-post)
+            (make-editor (rest one-more-on-pre) (cons (first one-more-on-pre)
+                                                      current-post))
+            ))]))
+
+(check-expect (split-structural
+               '()
+               0)
+              (make-editor
+               '()
+               '()
+               ))
+
+(check-expect (split-structural
+               '("h")
+               0)
+              (make-editor
+               '()
+               '("h")
+               ))
+
+(check-expect (split-structural
+               '("h" "e")
+               0)
+              (make-editor
+               '()
+               '("h" "e")
+               ))
+
+(check-expect (split-structural
+               '("h" "e" "l")
+               (add1 (image-width (editor-text '("h")))))
+              (make-editor
+               '("h")
+               '("e" "l")
+               ))
+
+(check-expect (split-structural
+               '("h" "e" "l" "l" "o")
+               (add1 (image-width (editor-text '("h" "e" "l")))))
+              (make-editor
+               '("l" "e" "h")
+               '("l" "o")
+               ))
+
