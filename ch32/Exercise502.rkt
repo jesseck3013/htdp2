396b01e47dfd0f12b3a28933b0d316cf29b643e2
jesseck3013 <jesseck3013@outlook.com>
2025-08-13 12:45:17 +0800

    added 502

---
+++ ch32/Exercise502.rkt
@@ -0,0 +1,19 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercise489) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; [NEList-of 1String] -> [NEList-of 1String]
+(define (palindrom l0)
+  (local (
+          (define (palindrom/acc los acc)
+            (cond
+              [(empty? (rest los)) (cons (first los) acc)]
+              [else (cons (first los)
+                          (palindrom/acc
+                           (rest los)
+                           (cons (first los) acc)))]))
+          )
+    (palindrom/acc l0 '())))
+
+(check-expect
+  (palindrom (explode "abc")) (explode "abcba"))
