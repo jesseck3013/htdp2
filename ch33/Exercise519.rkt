055eb583163b7902d53a17d3227adf34bd0069b7
jesseck3013 <jesseck3013@outlook.com>
2025-08-14 10:46:00 +0800

    added 519

---
+++ ch33/Exercise519.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |33.1|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp")) #f)))
+
+; The extra cost is memory usage. Whether it is acceptable depends on the actual use cases and the system that runs the program.
+
+; 1. Suppose length is barely used, then it is not worth to make the change.
+
+; 2. Suppose the program should be run on a system that has less memory, then we have to rethink if the system can support the new implementation.
+
